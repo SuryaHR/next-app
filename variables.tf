07eaf7e3dfc88825fcc4d0e757711929d05f8527
Surya H R <101381585+SuryaHR@users.noreply.github.com>
2024-09-05 17:34:54 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  description = "AKIAXYKJU3XLICZIF3OR"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "cHdnzZxjvwuovicipKQ34fcF/KGp2mXEzu4XpSyu"
+  type        = string
+}
