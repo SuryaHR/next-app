f43841132ac93a48b9b779808b13e19764ca02e5
Surya H R <101381585+SuryaHR@users.noreply.github.com>
2024-09-05 17:43:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,1 @@
-variable "aws_access_key" {
-  description = "AKIAXYKJU3XLICZIF3OR"
-  type        = string
-}
 
-variable "aws_secret_key" {
-  description = "cHdnzZxjvwuovicipKQ34fcF/KGp2mXEzu4XpSyu"
-  type        = string
-}
